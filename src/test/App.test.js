8e19dc84e840f47b74b76fe5c391716f5f8397a1
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-10-14 16:56:36 +0100

    gabarito

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,4 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
 
 describe('Testa aplicação', () => {
-  // Escreva os testes aqui
+  test('verifica se o input é renderizado', () => {
+    render(<App />);
+    const inputElement = screen.getByLabelText('Digite uma cor:');
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  test('verifica se o botão é renderizado', () => {
+    render(<App />);
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  test('se a cor digitada no input é enviada para uma `li`', () => {
+    render(<App />)
+    const inputElement = screen.getByLabelText('Digite uma cor:');
+    expect(inputElement).toBeInTheDocument();
+    userEvent.type(inputElement, 'green');
+
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toBeInTheDocument();
+    userEvent.click(buttonElement);
+
+    const colorElement = screen.getByTestId('color');
+    expect(colorElement).toBeInTheDocument()
+  });
 });
